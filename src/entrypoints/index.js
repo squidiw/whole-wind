fca726c8c68e510ef6b3da2f9df60b26cf8ee3ef
Koranteng Anane <squidiw.squidiw@gmail.com>
2023-06-26 18:36:47 -0400

    Edit 1.0.3
    1. Annoucement banner added
    
    2. SplideJS added for announcement banner

--- src/entrypoints/index.js
+++ src/entrypoints/index.js
@@ -1,3 +1,13 @@
+// SplideJS 
+import '@splidejs/splide/css';
+import Splide from '@splidejs/splide';
+let elms = document.getElementsByClassName( 'splide' );
+
+for ( var i = 0; i < elms.length; i++ ) {
+  new Splide( elms[ i ] ).mount();
+}
+// End SplideJS 
+
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
