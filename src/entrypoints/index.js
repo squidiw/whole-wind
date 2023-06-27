8b530ef66b01137ecd062f190da7455f5edc5b5a
Koranteng Anane <squidiw.squidiw@gmail.com>
2023-06-27 11:18:07 -0400

    Edit 1.0.4
    Changed splideJS setup from import to added script and link in <head>

--- src/entrypoints/index.js
+++ src/entrypoints/index.js
@@ -1,13 +1,3 @@
-// SplideJS 
-import '@splidejs/splide/css';
-import Splide from '@splidejs/splide';
-let elms = document.getElementsByClassName( 'splide' );
-
-for ( var i = 0; i < elms.length; i++ ) {
-  new Splide( elms[ i ] ).mount();
-}
-// End SplideJS 
-
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
