8359fe5725f433de5faa9761a1d49b9e88886ae7
Koranteng Anane <squidiw.squidiw@gmail.com>
2023-06-28 10:26:16 -0400

    Edit 1.0.0
    1. Formatted preload & link to assets code for production assets in theme.liquid
    
    2. Fixed content array with tailwind not finding the tailwind classes
    
    3. Created header.liquid file
    
    4. Created snippets for hamburger menu and close icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', './layout/**/*.{html,js}', './sections/**/*.{html,js}', './snippets/**/*.{html,js}', './templates/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}', './layout/**/*.{liquid,json}', './sections/**/*.{liquid,json}', './snippets/**/*.{liquid,json}', './templates/**/*.{liquid,json}'],
   theme: {
     extend: {},
   },
