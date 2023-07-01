af5232d11445f14fc1be8ffc8a5b65faaacd6ac8
Koranteng Anane <squidiw.squidiw@gmail.com>
2023-07-01 16:15:43 -0400

    Edit 1.0.6
    1. Added setting for desktop menu
    
    2. Megamenu template
    
    3. Added custom breakpoints for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{html,js,css}', './layout/**/*.{liquid,json}', './sections/**/*.{liquid,json}', './snippets/**/*.{liquid,json}', './templates/**/*.{liquid,json}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '375px',
+        'sm': '670px',
+        'md': '769px',
+        'lg': '1024px',
+        '1080': '1080px',
+        'xl': '1200px',
+        '2xl': '1320px',
+        '3xl': '1440px'
+      }
+    },
   },
   plugins: [],
 }
