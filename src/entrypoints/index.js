c6587f6f4f1d5900ca8132bae21fa7bd1b772906
Koranteng Anane <squidiw.squidiw@gmail.com>
2023-07-12 13:22:36 -0400

    Edit 1.0.0
    1. Added main-cart file
    
    2. Edited cart.json to use the right file for cart page
    
    3. Added base template for main-cart file

--- src/entrypoints/index.js
+++ src/entrypoints/index.js
@@ -13,29 +13,5 @@ window._arrayEquals = function arrayEquals(a, b){
 
 
 Alpine.data('product', () => {
-  return {
-    product: JSON.parse(document.querySelector('[data-product-json]').innerHTML),
-    selectedVariant: null,
-    quantity: 1,
-    
-    
-    init() {
-      console.log('init')
-      this.product = JSON.parse(document.querySelector('[data-product-json]').innerHTML)
-      
-      const params = (new URL(document.location)).searchParams
-      const variant_id = params.get('variant_id')
-      let firstAvailableVariant = this.product.variants.find(v => v.available === true) || this.product.variants[0]
-
-      if(variant_id !== '' && this.product.variants.find(v => v.id === Number(variant_id)) !== undefined){
-        const variantFromUrlParams = this.product.variants.find(v => v.id === Number(variant_id))
-        if(variantFromUrlParams.available){
-          firstAvailableVariant = variantFromUrlParams
-        }
-      }
-
-      this.selectedVariant = firstAvailableVariant
-
-    }
-  }
+  
 })
